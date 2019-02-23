76d6bec9b14e0475afc2a2aa4779a2ff6033774f
s-ong-c <somony9292@gmail.com>
2019-02-23 19:45:29 +0900

    card & board  추가

---
+++ src/components/Board.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        Board
+        <div>bid: {{bid}}</div>
+        <hr />
+        <router-view></router-view>
+    </div>
+</template>
+<script>
+export default {
+    data(){
+        return{
+            bid:0
+        }
+    },
+    created() {
+        //this.$route route정보 get 
+        this.bid = this.$route.params.bid
+    }
+}
+</script>
+<style>
+    
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,20 @@ import VueRouter from 'vue-router'
 import Home from '../components/Home.vue'
 import Login from '../components/Login.vue'
 import NotFound from '../components/NotFound.vue'
-
+import Board from '../components/Board.vue'
+import Card from '../components/Card.vue'
 Vue.use(VueRouter)
 
-// const Login ={template:'<div>LOGIN PAGE</div>'}
-// const NotFound= { template :'<div>PAGE NOT FOUND</div>'}
-// const routes ={
-//   '/':App,
-//   '/login':Login
-// }
-// 라우트 정의
 
-// 3. `routes` 옵션과 함께 router 인스턴스를 만드세요.
 
 const router = new VueRouter({
   mode:'history',  // /#/해쉬 백 나오지 않게 하기위해서 설정 
   routes :[
     { path: '/', component: Home },
     { path: '/login', component: Login },
+    { path:'/b/:bid',component: Board , children:[
+        { path:'c/:cid',component:Card}
+    ]   },
     { path: '*', component: NotFound }
   ]
 })
