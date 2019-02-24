c91cb8a732365872b3f09c27a05052ed06df50c5
s-ong-c <somony9292@gmail.com>
2019-02-24 23:45:46 +0900

    beforeEnter:requireAuth 설정

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,19 @@ import Board from '../components/Board.vue'
 import Card from '../components/Card.vue'
 Vue.use(VueRouter)
 
-
+const requireAuth = (to,from,next)=>{
+  const isAuth = localStorage.getItem('token')
+  const loginPath = `/login?rPath=${encodeURIComponent(to.path)}`
+  isAuth ? next() : next(loginPath)
+}
 
 const router = new VueRouter({
   mode:'history',  // /#/해쉬 백 나오지 않게 하기위해서 설정 
   routes :[
-    { path: '/', component: Home },
+    { path: '/', component: Home, beforeEnter:requireAuth },
     { path: '/login', component: Login },
-    { path:'/b/:bid',component: Board , children:[
-        { path:'c/:cid',component:Card}
+    { path:'/b/:bid',component: Board, beforeEnter:requireAuth , children:[
+        { path:'c/:cid',component:Card, beforeEnter:requireAuth}
     ]   },
     { path: '*', component: NotFound }
   ]
