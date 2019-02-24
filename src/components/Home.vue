db09b1b3904b9df039f728c7011cf8438f7fee25
s-ong-c <somony9292@gmail.com>
2019-02-24 23:07:05 +0900

    axios 추가 및 axios 연동

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -4,9 +4,8 @@
     <div>
         Board List:
         <div v-if ="loading">Loading ...</div>
-        <div v-else>
-        Api reusult : {{apiRes}}
-        </div>
+        <div v-else>Api reusult : <pre>{{apiRes}}</pre></div>
+        <div v-if="error"><pre>{{error}}</pre></div>
         <ul>
             <li>
                 <router-link to="/b/1">Board 1</router-link>
@@ -19,11 +18,13 @@
     </div>
 </template>
 <script>
+import axios from 'axios'
 export default {
     data(){
         return{
             loading:false,
-            apiRes:''
+            apiRes:'',
+            error:''
         }
     },
     created() {
@@ -32,7 +33,18 @@ export default {
     methods: {
         fetchData(){
             this.loading = true
-            
+
+            axios.get('http://localhost:3000/h_ealth')
+                .then(res =>{
+                    this.apiRes  = res.data
+                })
+                .catch(res =>{
+                    this.error = res.response.data
+                })
+                .finally(()=>{
+                    this.loading = false
+                })
+            /*
             const req = new XMLHttpRequest()
 
             req.open('GET','http://localhost:3000/health')
@@ -47,6 +59,7 @@ export default {
                     response:JSON.parse(req.response)
                 }
             })
+            */
         }
     },
 }
