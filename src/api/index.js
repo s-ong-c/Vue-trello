a6ca79a9067995ad5f0a79d65a35982357b0b74d
s-ong-c <somony9292@gmail.com>
2019-02-26 16:30:40 +0900

    card modal 조회

--- src/api/index.js
+++ src/api/index.js
@@ -36,6 +36,9 @@ export const board = {
 export const card = {
   create(title, listId, pos) {
     return request('post', '/cards', {title, listId, pos})
+  },
+  fetch(id){
+    return request('get',`/cards/${id}`)
   }
 }
 
--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,45 +1,64 @@
 <template>
-    <Modal>
-        Card
-        <div v-if="loading">loading card ....</div>
-        <div v-else>cid : {{cid}}</div>
-       
-    </Modal>
+  <Modal class="modal-card">
+    <div slot="header" class="modal-card-header">
+      <div class="modal-card-header-title">
+        <input class="form-control" type="text" :value="card.title" readonly>
+      </div>
+      <a class="modal-close-btn" href="" @click.prevent="onClose">&times;</a>
+    </div>
+    <div slot="body">
+      <h3>Description</h3>
+      <textarea  class="form-control" cols="30" rows="3" placeholder="Add a more detailed description..."
+        readonly
+        v-model="card.description"></textarea>
+    </div>
+    <div slot="footer"></div>
+  </Modal>
 </template>
+
 <script>
 import Modal from './Modal.vue'
+import {mapState, mapActions} from 'vuex'
 export default {
-    components:{
-        Modal
-    },
-    data(){
-        return{
-        cid:0,
-        loading: false
-        }
-    },
-    watch:{
-        '$route':{
-            handler: 'fetchData',
-            immediate : true
-            // this.cid = this.$route.params.cid
-          //  this.fetchData()
-        }
-    },
-    // created(){
-    //     this.fetchData()
-    // },
-    methods: {
-        fetchData(){
-            this.loading= true
-            setTimeout(()=>{
-             this.cid = this.$route.params.cid
-             this.loading = false
-            },500)
-        }
+  components: {Modal},
+  computed: {
+    ...mapState({
+      card: 'card',
+      board: 'board'
+    })
+  },
+  created() {
+    const id = this.$route.params.cid
+    this.FETCH_CARD({id})
+  },
+  methods: {
+    ...mapActions([
+      'FETCH_CARD'
+    ]),
+    onClose() {
+      this.$router.push(`/b/${this.board.id}`)
     }
+  }
 }
 </script>
+
 <style>
-    
+.modal-card .modal-container {
+  min-width: 300px;
+  max-width: 800px;
+  width: 60%;
+}
+.modal-card-header-title {
+  padding-right: 30px;  
+}
+.modal-close-btn {
+  position: absolute;
+  top: 0px;
+  right: 0px;
+  font-size: 24px;
+  text-decoration: none;
+}
+.modal-card-header {
+  position: relative;
+}
 </style>
